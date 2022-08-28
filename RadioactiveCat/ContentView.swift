ac2e2bd9d9feed76e3048d96831ac71bb4164cc2
Ellen Shapiro <designatednerd@gmail.com>
2022-08-28 10:26:00 -0600

    Initial Commit

---
+++ RadioactiveCat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RadioactiveCat
+//
+//  Created by Ellen Shapiro on 8/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RadioactiveCat/RadioactiveCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioactiveCatApp.swift
+//  RadioactiveCat
+//
+//  Created by Ellen Shapiro on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RadioactiveCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
