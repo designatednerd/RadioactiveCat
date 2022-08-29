53d5b7aea99e32428a0e7dd88d8103290c528aa0
Ellen Shapiro <designatednerd@gmail.com>
2022-08-29 16:41:07 -0600

    Attempt to run build tool plugin on main target, get error, remove from build tool plugins list on main target

--- RadioactiveCat/Packages/Cats/Package.swift
+++ RadioactiveCat/Packages/Cats/Package.swift
@@ -10,24 +10,15 @@ let package = Package(
         .iOS("13.0")
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Cats",
             targets: ["Cats"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/designatednerd/DatedImageGenerator.git", branch: "main"),
-        
-        // Temp until fix gets merged
-        .package(url: "https://github.com/designatednerd/DoNilDisturbPlugin.git", branch: "main")
-//        .package(url: "https://github.com/icanzilb/DoNilDisturbPlugin.git", from: "0.0.2"),
-
+        .package(url: "https://github.com/icanzilb/DoNilDisturbPlugin.git", from: "0.0.5"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Cats",
             dependencies: [
